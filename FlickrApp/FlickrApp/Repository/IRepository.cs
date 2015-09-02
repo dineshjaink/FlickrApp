1448e72ce72840c911bb779d87c46a827d89995c
dineshjaink <dinesh.jain.kumar@gmail.com>
2015-09-02 22:05:13 +1000

    Working Copy
    Issues:
    1. Zoom Issue
    2. Design Fixes

---
+++ FlickrApp/FlickrApp/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using FlickrApp.Models;
+
+namespace FlickrApp.Repository
+{
+    /// <summary>
+    /// The Repository interface
+    /// </summary>
+    public interface IRepository
+    {
+        List<FlickrImage> GetImagesByTags(string tags);
+    }
+}
---
+++ FlickrApp/FlickrApp/Scripts/script.js
@@ -0,0 +1,71 @@
+﻿
+var tags = null;
+
+jQuery("#tags").keyup(function(e) {
+    if (e.which == 13) {
+
+        clearTimeout(tags);
+        tags = setTimeout(function() {
+            if (jQuery("#tags").val().length < 3) {
+                return false;
+            }
+
+            var url = window.location + "Home/GetImages?tags=" + jQuery("#tags").val();
+
+
+            jQuery.post(url, function(data) {
+                var container = jQuery("#main");
+                container.html("");
+
+                if (data != null) {
+                    for (var i = document.images.length; i-- > 0;)
+                        document.images[i].parentNode.removeChild(document.images[i]);
+                };
+
+                var index = 0;
+
+                jQuery.each(data, function() {
+
+                    index++;
+                    
+                    var thumb = jQuery(".imgResult").fadeIn(5000);
+
+                    var imageDiv = document.createElement("div");
+
+                    imageDiv.id = "divImg" + index;
+                    imageDiv.setAttribute('class', 'imgContent');
+                    var image = document.createElement("img");
+                    image.alt = "Flickr Search";
+                    image.id = 'Image' + index;
+                    image.setAttribute('class', 'images');
+                    image.src = this.ImageUrl;
+                    image.title = this.Title;
+                    //image.fadeOut(5000);
+
+                    imageDiv.appendChild(image);
+
+                    thumb.append(imageDiv);
+
+                    container.html(thumb);
+
+                    jQuery('#Image' + index).imageMagnify({
+                        magnifyby: 1.5
+                    });
+                    jQuery(".images").fadeIn(20000, function() {
+                        // Animation complete.
+                    });
+                });
+            });
+        }, 250);
+    }
+});
+
+jQuery(document).ajaxStart(function () {
+    jQuery("#content").show();
+});
+
+jQuery(document).ajaxStop(function () {
+    jQuery("#content").hide();
+});
+
+
